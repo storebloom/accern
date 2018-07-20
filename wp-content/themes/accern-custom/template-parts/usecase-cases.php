b582eb040672ba7d061ec90f8acfdf1535c80e01
Dustin Jones <info@domesticjones.com>
2018-07-20 15:17:30 -0600

    Add transition delays to header menu, remove visible Y-axis hiding on menu initiation, style header and nav menu for mobile/responsive, replace header logo with SVG referencing dist folder

--- wp-content/themes/accern-custom/template-parts/usecase-cases.php
+++ wp-content/themes/accern-custom/template-parts/usecase-cases.php
@@ -14,42 +14,35 @@ $args = array(
 );
 
 $use_cases = get_posts( $args );
+?>
+<div id="usecase-cases-section">
+	<div class="usecase-list">
+		<?php
+		foreach ( $use_cases as $case ) :
+			$case_info = get_post_meta( $case->ID, 'page-meta', true );
+			$case_info = ! empty( $case_info ) ? $case_info : array();
+			$tabs = isset( $case_info['usecase-tab-section']['usecase-repeater'] ) ? $case_info['usecase-tab-section']['usecase-repeater'] : '';
 
-foreach ( $use_cases as $case ) :
-	$case_info = get_post_meta( $case->ID, 'page-meta', true );
-	$case_name[] = $case->post_title;
-	$case_info = ! empty( $case_info ) ? $case_info : array();
-	$tabs = isset( $case_info['usecase-tab-section']['usecase-repeater'] ) ? $case_info['usecase-tab-section']['usecase-repeater'] : '';
-	?>
-	<div class="usecase-section">
-		<a name="<?php echo esc_attr( $case->post_title ); ?>"></a>
+			if ( array() !== $case_info ) : ?>
+				<div class="section-title">
+					<?php echo esc_html( $case->post_title ); ?>
+				</div>
+				<div class="section-sub-title">
+					<?php echo esc_html( $case_info['usecase-main-section']['sub-title'] ); ?>
+				</div>
+			<?php
+				include( get_template_directory() . '/theme-templates/use-case.php' );
 
-		<?php if ( array() !== $case_info ) : ?>
-			<div class="section-title">
-				<?php echo esc_html( $case->post_title ); ?>
-			</div>
-			<div class="section-sub-title">
-				<?php echo esc_html( $case_info['usecase-main-section']['sub-title'] ); ?>
-			</div>
-			<div class="usecase-tab-content">
-				<?php include( get_template_directory() . '/theme-templates/use-case.php' ); ?>
-			</div>
-			<div class="usecase-tabs">
-				<?php
-				foreach ( $tabs as $num => $tab_info ) :
-						$tab_id = strtolower( str_replace( ' ', '-', $tab_info['title'] ) );
-					?>
-						<li data-tab="<?php echo esc_attr( $tab_id ); ?>" class="tab-item">
-							<?php echo esc_html( $tab_info['title'] ); ?>
-						</li>
-				<?php endforeach; ?>
-			</div>
-		<?php endif; ?>
+			foreach ( $tabs as $num => $tab_info ) :
+					$tab_id = strtolower( str_replace( ' ', '-', $tab_info['title'] ) );
+				?>
+					<li data-tab="<?php echo esc_attr( $tab_id ); ?>" class="tab-item">
+						<?php echo esc_html( $tab_info['title'] ); ?>
+					</li>
+		<?php
+				endforeach;
+			endif;
+		endforeach;
+		?>
 	</div>
-<?php endforeach; ?>
-
-<?php
-$page_names = array_unshift( $case_name, $section_info['title'] );
-
-// Nav icons.
-get_accern_nav_icons( $id, count( $use_cases ) + 1, 'usecase', $page_names, true );
+</div>
